0c7292b0740966fdc3129d54f561ceb7eebf2bcb
Nick <nicolasw.palmer@gmail.com>
2018-11-29 23:54:09 -0500

    fixed formatting and closing bracket error

--- public/js/main.js
+++ public/js/main.js
@@ -7,38 +7,39 @@ $(function () {
 		chatter_count = response.length;
 	});
 
-$('#join-chat').click(function() {
-	var username = $.trim($('#username').val());
-	$.ajax({
-		url: '/join',
-		type: 'POST',
-		data: {
-			username: username
-		},
-		success: function(response) {
-			if (response.status == 'OK') {
-				socket.emit('update_chatter_count', {
-					'action': 'increase'
-				});
-				$('.chat').show();
-				$('#leave-chat').data('username', username);
-				$('#send-message').data('username', username);
-				$.get('/get_messages', function(response) {
-					if (response.length > 0) {
-						var message_count = response.length;
-						var html = '';
-						for (var x = 0; x < message_count; x++) {
-							html += "<div class='msg'><div class='user'>" + response[x]['sender'] + "</div><div class='txt'>" + response[x]['message'] + "</div></div>";
+	$('#join-chat').click(function() {
+		var username = $.trim($('#username').val());
+		$.ajax({
+			url: '/join',
+			type: 'POST',
+			data: {
+				username: username
+			},
+			success: function(response) {
+				if (response.status == 'OK') {
+					socket.emit('update_chatter_count', {
+						'action': 'increase'
+					});
+					$('.chat').show();
+					$('#leave-chat').data('username', username);
+					$('#send-message').data('username', username);
+					$.get('/get_messages', function(response) {
+						if (response.length > 0) {
+							var message_count = response.length;
+							var html = '';
+							for (var x = 0; x < message_count; x++) {
+								html += "<div class='msg'><div class='user'>" + response[x]['sender'] + "</div><div class='txt'>" + response[x]['message'] + "</div></div>";
+							}
+							$('.messages').html(html);	
 						}
-						$('.messages').html(html);	
-					}
-				});
-				$('.join-chat').hide();
-			} else if (response.status == 'FAILED') {
-				alert("That username already exists, please try another");
-				$('#username').val('').focus();
+					});
+					$('.join-chat').hide();
+				} else if (response.status == 'FAILED') {
+					alert("That username already exists, please try another");
+					$('#username').val('').focus();
+				}
 			}
-		}
+		});
 	});
 
 	$('#leave-chat').click(function() {
